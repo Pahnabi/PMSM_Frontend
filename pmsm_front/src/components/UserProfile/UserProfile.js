6a39a63492aff627bc52192138cfe01ec3fb026c
DikshantSh <dikshant.2412@kgpian.iitkgp.ac.in>
2024-02-01 02:07:10 +0530

    .

--- pmsm_front/src/components/UserProfile/UserProfile.js
+++ pmsm_front/src/components/UserProfile/UserProfile.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./UserProfile.css";
 
 const UserProfile = () => {
+  const batteryLevel = 70;
   return (
-    <div>
-      <h1>User</h1>
+    <div className="user-page">
+      <div className="row0"></div>
+      <div className="row1">
+        <div className="box0">{/* Content for the first box */}</div>
+        <div className="box1">
+          <div className="battery-indicator">
+            <div
+              className="battery-level"
+              style={{ width: `${batteryLevel}%` }}
+            ></div>
+          </div>
+        </div>
+        <div className="box2">{/* Content for the second box */}</div>
+      </div>
+      <div className="row2">
+        <div className="box0">{/* Content for the first box */}</div>
+        <div className="box3">{/* Content for the third box */}</div>
+        <div className="box4">{/* Content for the fourth box */}</div>
+      </div>
+      <div className="row0"></div>
     </div>
-  )
-}
+  );
+};
 
-export default UserProfile
+export default UserProfile;
