3a9dea5941e187faf25147ad539a9c8991941c64
MasterEzio <sushant.singh.7326@gmail.com>
2024-02-08 04:00:25 +0530

    footer ready

--- pmsm_front/src/components/Footer/Footer.js
+++ pmsm_front/src/components/Footer/Footer.js
@@ -35,17 +35,40 @@ function Footer() {
     //   </div>
     // </div>
     <div className="footer">
-      <div className="footer-top-logo">
-        {/* <img src="https://placekitten.com/300/300" alt="" />
-        <img src="https://placekitten.com/300/300" alt="" /> */}
-        <img
-          src="https://img1.wsimg.com/isteam/ip/f6c7533f-74be-48e9-a0e9-b5e7fefa8122/blob-3f78b51.png/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:600,cg:true"
-          alt=""
-        />
+      <div className="footer-left">
+        <h1 className="footer-heading text-2xl font-extrabold font-mono text-center text-white">
+          Contact Us
+        </h1>
+        <form action="">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email ID" />
+          <input type="tel" placeholder="Phone" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            // rows="8"
+            placeholder="Message"
+            maxLength={200}
+            style={{ height: "100px", resize: "none" }}
+          ></textarea>
+          <div className="footer-form-buttons">
+            <input type="reset" />
+            <input type="submit" value="Send" />
+          </div>
+        </form>
       </div>
-      <div className="footer-cr">
-        <div className="copyright text-white text-center mt-4">
-          <p>&copy; 2024 PMSM IIT KGP. All rights reserved.</p>
+      <div className="footer-right">
+        <div className="footer-top-logo">
+          <img
+            src="https://img1.wsimg.com/isteam/ip/f6c7533f-74be-48e9-a0e9-b5e7fefa8122/blob-3f78b51.png/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:600,cg:true"
+            alt=""
+          />
+        </div>
+        <div className="footer-cr">
+          <div className="copyright text-white text-center mt-4">
+            <p>&copy; 2024 PMSM IIT KGP. All rights reserved.</p>
+          </div>
         </div>
       </div>
     </div>
