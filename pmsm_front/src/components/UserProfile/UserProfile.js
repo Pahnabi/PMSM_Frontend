5060d8834c0e07fe1a435bafb0a3bd8f4d34ad92
MasterEzio <sushant.singh.7326@gmail.com>
2024-02-08 14:45:38 +0530

    previous footer push, evhealth percent on profile

--- pmsm_front/src/components/UserProfile/UserProfile.js
+++ pmsm_front/src/components/UserProfile/UserProfile.js
@@ -3,6 +3,12 @@ import "./UserProfile.css";
 
 const UserProfile = () => {
   const batteryLevel = 70;
+  const inlineStyles = {
+    stroke: `#8bc34a`,
+    strokedashoffset: `calc(440 - (440 * 69) / 100)`,
+    // animation: animate 1s linear forwards;
+  };
+
   return (
     <div className="user-page">
       <div className="row0"></div>
@@ -16,7 +22,20 @@ const UserProfile = () => {
             ></div>
           </div>
         </div>
-        <div className="box2">{/* Content for the second box */}</div>
+        <div className="box2">
+          {/* Content for the second box */}
+          <div className="evhealth-percent">
+            <svg>
+              <circle cx="70" cy="70" r="70"></circle>
+              <circle cx="70" cy="70" r="70"></circle>
+            </svg>
+            <div className="evhealth-number">
+              <h2>
+                60 <span>%</span>
+              </h2>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="row2">
         <div className="box0">{/* Content for the first box */}</div>
