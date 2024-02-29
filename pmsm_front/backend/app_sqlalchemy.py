418f8b57f5db955dcf8160be42a8b1920e8f354f
DikshantSh <dikshant.2412@kgpian.iitkgp.ac.in>
2024-02-29 09:12:21 +0530

    .

--- pmsm_front/backend/app_sqlalchemy.py
+++ pmsm_front/backend/app_sqlalchemy.py
@@ -53,7 +53,7 @@ def add_current_data_periodically():
 
 @app.route('/current-data', methods=['GET'])
 def get_current_data():
-    current_data = CurrentData.query.order_by(CurrentData.id.desc()).limit(5).all()
+    current_data = CurrentData.query.order_by(CurrentData.id.desc()).limit(20).all()
     data = [{'current': data.current, 'timestamp': data.timestamp} for data in current_data]
     return jsonify(data)
 
