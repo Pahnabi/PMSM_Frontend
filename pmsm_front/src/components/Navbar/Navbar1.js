a66dbcf2e1efab9bb95e1641c8bf1c416d7f0524
soumili202 <soumilirupsa2020@gmail.com>
2024-02-29 11:50:05 +0530

    made navbar responsive

--- pmsm_front/src/components/Navbar/Navbar1.js
+++ pmsm_front/src/components/Navbar/Navbar1.js
@@ -6,21 +6,29 @@ import { useNavigate } from "react-router-dom";
 function Navbar1(props) {
   const navigate = useNavigate();
   const [scrolling, setScrolling] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 50) {
-        // alert("hi");
         setScrolling(true);
       } else {
         setScrolling(false);
       }
     };
 
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize(); // Initial check on component mount
     window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -31,65 +39,127 @@ function Navbar1(props) {
     });
   };
 
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+    console.log(sidebarOpen);
+  };
+
   return (
     <div className={`navbar1 ${scrolling ? "solid" : "transparent"}`}>
       <div className="left-section">
         <img src="./Media/kgp_logo.png" alt="Logo" className="logo" />
         <h1 className="navbar-content-heading" onClick={scrollToTop}>AI4ICPS</h1>
       </div>
-      {!props.loginstatus &&(
-      <div className="right-section">
-        <ul>
-          <li>
-            <Link to="home" onClick={scrollToTop}>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="about" spy={true} smooth={true} duration={500}>
-              About
-            </Link>
-          </li>
-          <li>
-            <Link to="services" spy={true} smooth={true} duration={500}>
-              Services
-            </Link>
-          </li>
-          <li>
-            <Link to="client" spy={true} smooth={true} duration={500}>
-              Client
-            </Link>
-          </li>
-          <li>
-            <Link to="team" spy={true} smooth={true} duration={500}>
-              Team
-            </Link>
-          </li>
-          <li>
-            <button
-              onClick={() => {
-                props.setOpenModal();
-              }}
-            >
-              Login
-            </button>
-          </li>
-        </ul>
-      </div>)}
-      {props.loginstatus &&(
-      <div className="right-section">
-        <ul>
-          <li>
-            <button
-              onClick={() => {
-                navigate("/");
-              }}
-            >
-              Logout
-            </button>
-          </li>
-        </ul>
-      </div>)}
+      {isMobile && (
+        <div className="menu-toggle" onClick={toggleSidebar}>
+          <i className="fas fa-bars"></i>
+        </div>
+      )}
+      {isMobile ? (
+        <div className={`sidebar ${sidebarOpen ? "open" : ""}`}>
+          <ul>
+            <li>
+              <Link to="home" onClick={scrollToTop}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="about" spy={true} smooth={true} duration={500}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="services" spy={true} smooth={true} duration={500}>
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link to="client" spy={true} smooth={true} duration={500}>
+                Client
+              </Link>
+            </li>
+            <li>
+              <Link to="team" spy={true} smooth={true} duration={500}>
+                Team
+              </Link>
+            </li>
+            {!props.loginstatus && (
+              <li>
+                <button
+                  onClick={() => {
+                    props.setOpenModal();
+                  }}
+                >
+                  Login
+                </button>
+              </li>
+            )}
+            {props.loginstatus && (
+              <li>
+                <button
+                  onClick={() => {
+                    navigate("/");
+                  }}
+                >
+                  Logout
+                </button>
+              </li>
+            )}
+          </ul>
+        </div>
+      ) : (
+        <div className="right-section">
+          <ul>
+            <li>
+              <Link to="home" onClick={scrollToTop}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="about" spy={true} smooth={true} duration={500}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="services" spy={true} smooth={true} duration={500}>
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link to="client" spy={true} smooth={true} duration={500}>
+                Client
+              </Link>
+            </li>
+            <li>
+              <Link to="team" spy={true} smooth={true} duration={500}>
+                Team
+              </Link>
+            </li>
+            {!props.loginstatus && (
+              <li>
+                <button
+                  onClick={() => {
+                    props.setOpenModal();
+                  }}
+                >
+                  Login
+                </button>
+              </li>
+            )}
+            {props.loginstatus && (
+              <li>
+                <button
+                  onClick={() => {
+                    navigate("/");
+                  }}
+                >
+                  Logout
+                </button>
+              </li>
+            )}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
