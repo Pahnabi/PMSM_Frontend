dfbd06587c397d0b5b2b25b555cc01db0e35fcf5
MasterEzio <sushant.singh.7326@gmail.com>
2024-03-14 04:01:01 +0530

    created and initialised userprofile2

---
+++ pmsm_front/src/components/UserProfile/UserProfile2.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./UserProfile2.css";
+import Box3 from "./blocks/Box3";
+import CarImage from "./blocks/CarImage";
+import DashboardMap from "./blocks/DashboardMap";
+
+const UserProfile2 = () => {
+  const position = [51.505, -0.09];
+
+  return (
+
+    <div className="user-profile-2">
+      <div className="user-profile-2-card-1">
+        <CarImage />
+      </div>
+      <div className="user-profile-2-card-1">
+        <DashboardMap />
+      </div>
+      <div className="user-profile-2-card-1">
+        <Box3 />
+      </div>
+      <div className="user-profile-2-card-1"></div>
+    </div>
+  );
+};
+
+export default UserProfile2;
