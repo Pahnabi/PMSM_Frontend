c7dcd225dce1202fbe095a2c57f28d221a70d115
DikshantSh <dikshant.2412@kgpian.iitkgp.ac.in>
2024-03-14 00:19:00 +0530

    footer

--- pmsm_front/src/components/Footer/Footer.js
+++ pmsm_front/src/components/Footer/Footer.js
@@ -34,44 +34,104 @@ function Footer() {
     //     <p>&copy; 2024 PMSM IIT KGP. All rights reserved.</p>
     //   </div>
     // </div>
-    <div className="footer">
-      <div className="footer-left">
-        <h1 className="footer-heading text-2xl font-extrabold font-mono text-center text-white">
-          Contact Us
-        </h1>
-        <form action="">
-          <input type="text" placeholder="Your Name" />
-          <input type="email" placeholder="Email ID" />
-          <input type="tel" placeholder="Phone" />
-          <textarea
-            name=""
-            id=""
-            cols="30"
-            // rows="8"
-            placeholder="Message"
-            maxLength={200}
-            style={{ height: "100px", resize: "none" }}
-          ></textarea>
-          <div className="footer-form-buttons">
-            <input type="reset" />
-            <input type="submit" value="Send" />
-          </div>
-        </form>
+
+    // <div className="footer">
+    //   <div className="footer-left">
+    //     <h1 className="footer-heading text-2xl font-extrabold font-mono text-center text-white">
+    //       Contact Us
+    //     </h1>
+    //     <form action="">
+    //       <input type="text" placeholder="Your Name" />
+    //       <input type="email" placeholder="Email ID" />
+    //       <input type="tel" placeholder="Phone" />
+    //       <textarea
+    //         name=""
+    //         id=""
+    //         cols="30"
+    //         // rows="8"
+    //         placeholder="Message"
+    //         maxLength={200}
+    //         style={{ height: "100px", resize: "none" }}
+    //       ></textarea>
+    //       <div className="footer-form-buttons">
+    //         <input type="reset" />
+    //         <input type="submit" value="Send" />
+    //       </div>
+    //     </form>
+    //   </div>
+    //   <div className="footer-right">
+    //     <div className="footer-top-logo">
+    //       <img
+    //         src="https://img1.wsimg.com/isteam/ip/f6c7533f-74be-48e9-a0e9-b5e7fefa8122/blob-3f78b51.png/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:600,cg:true"
+    //         alt=""
+    //       />
+    //     </div>
+    //     <div className="footer-cr">
+    //       <div className="copyright text-white text-center mt-4">
+    //         <p>&copy; 2024 PMSM IIT KGP. All rights reserved.</p>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <footer class="footer-main p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
+      <div class="mx-auto max-w-screen-xl text-center">
+        <a
+          href="#"
+          class="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white"
+        >
+          <span class="material-symbols-outlined">satellite_alt</span>
+          SmartEV
+        </a>
+        <p class="my-6 text-gray-500 dark:text-gray-400">
+          Redefining the Future of Electric Transportation.
+        </p>
+        <ul class="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6 ">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">
+              Premium
+            </a>
+          </li>
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6 ">
+              Initiatives
+            </a>
+          </li>
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">
+              Blog
+            </a>
+          </li>
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">
+              Affiliate Program
+            </a>
+          </li>
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">
+              FAQs
+            </a>
+          </li>
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">
+              Contact
+            </a>
+          </li>
+        </ul>
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2024{" "}
+          <a href="#" class="hover:underline">
+            SmartEV™
+          </a>
+          . All Rights Reserved.
+        </span>
       </div>
-      <div className="footer-right">
-        <div className="footer-top-logo">
-          <img
-            src="https://img1.wsimg.com/isteam/ip/f6c7533f-74be-48e9-a0e9-b5e7fefa8122/blob-3f78b51.png/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:600,cg:true"
-            alt=""
-          />
-        </div>
-        <div className="footer-cr">
-          <div className="copyright text-white text-center mt-4">
-            <p>&copy; 2024 PMSM IIT KGP. All rights reserved.</p>
-          </div>
-        </div>
-      </div>
-    </div>
+    </footer>
   );
 }
 
