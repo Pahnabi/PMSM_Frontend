71677b6e1a98b58f89d9f8903cef9631875e2b51
MasterEzio <sushant.singh.7326@gmail.com>
2024-03-14 04:03:57 +0530

    battery info - minor changes

--- pmsm_front/src/components/UserProfile/blocks/Box3.js
+++ pmsm_front/src/components/UserProfile/blocks/Box3.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./Box3.css";
-import BatteryComponent from "./Battery";
 
 const Box3 = () => {
     return(
         <div className="user-profile-box3">
-            <div className="charging-info"><BatteryComponent/> </div>
-            <div className="energy-usage"><BatteryComponent/> </div>
+            <div className="charging-info"></div>
+            <div className="energy-usage"> </div>
         </div>
     )
 };
