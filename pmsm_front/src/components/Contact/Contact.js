7896465ecf9406fb0d0738cd9322fc24b84368ed
DikshantSh <dikshant.2412@kgpian.iitkgp.ac.in>
2024-03-14 14:31:44 +0530

    .

--- pmsm_front/src/components/Contact/Contact.js
+++ pmsm_front/src/components/Contact/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
     <div className="contact-page">
       <section class="">
         <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+          <h2 class="contact-page-heading mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
             Contact Us
           </h2>
           <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
