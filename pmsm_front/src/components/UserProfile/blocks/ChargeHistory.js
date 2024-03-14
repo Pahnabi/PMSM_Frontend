0ad1225cd24416360bc31fca587e447c3d003a41
MasterEzio <sushant.singh.7326@gmail.com>
2024-03-14 13:56:12 +0530

    charge history -> block initialised

---
+++ pmsm_front/src/components/UserProfile/blocks/ChargeHistory.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import './ChargeHistory.css';
+// import data from './ChargeHistory.json'; // Import the JSON file
+
+const ChargeHistory = () => {
+  return (
+    <div className="data-card-container">
+      {/* {latestData.map((entry, index) => (
+        <div key={index} className="data-card">
+          <div className="date-time">
+            <span>{entry.date}</span>
+            <span>{entry.time}</span>
+          </div>
+          <div className="value">{entry.value}</div>
+        </div>
+      ))} */}
+    </div>
+  );
+};
+
+export default ChargeHistory;
