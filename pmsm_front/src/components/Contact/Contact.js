9a71b15686778af75fddd6efbe5f88cf25629ab0
DikshantSh <dikshant.2412@kgpian.iitkgp.ac.in>
2024-03-14 14:14:45 +0530

    .

--- pmsm_front/src/components/Contact/Contact.js
+++ pmsm_front/src/components/Contact/Contact.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Contact.css";
 
 function Contact() {
   return (
-    <div>
-      <section class="bg-white dark:bg-gray-900">
+    <div className="contact-page">
+      <section class="">
         <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
           <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
             Contact Us
