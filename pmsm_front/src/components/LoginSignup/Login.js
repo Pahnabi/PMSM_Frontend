96f10d64020ca499e67996b1e25618bfb3589c99
DikshantSh <dikshant.2412@kgpian.iitkgp.ac.in>
2024-03-19 17:56:30 +0530

    .

--- pmsm_front/src/components/LoginSignup/Login.js
+++ pmsm_front/src/components/LoginSignup/Login.js
@@ -22,7 +22,7 @@ const Login = (props) => {
     e.preventDefault();
     try {
       await axios
-        .post("http://10.29.8.31:5000:5000/login", {
+        .post("http://10.29.8.31:5000/login", {
           userid,
           vehicleid,
           password,
