7016cb41436f6b242bc3dee02b03cd5fcf4ca7e3
MasterEzio <sushant.singh.7326@gmail.com>
2024-03-21 03:29:43 +0530

    box3 changed flex to grid

--- pmsm_front/src/components/UserProfile/blocks/Box3.js
+++ pmsm_front/src/components/UserProfile/blocks/Box3.js
@@ -2,21 +2,39 @@ import React from "react";
 import "./Box3.css";
 import BatteryComponent from "./BatteryComponent";
 import EVHealth from "./EVHealth";
-import LastChargeInfo from "./LastChargeInfo";
+import WeatherData from "./WeatherData";
 import ChargeHistory from "./ChargeHistory";
 
 const Box3 = () => {
+  // return (
+  //   <div className="user-profile-box3">
+  //     <div className="battery-info">
+  //       <BatteryComponent />
+  //       <div className="evhealth">
+  //         <EVHealth healthvalue={80} />
+  //       </div>
+  //     </div>
+  //     <div className="energy-usage">
+  //       <WeatherData />
+  //       <ChargeHistory/>
+  //     </div>
+  //   </div>
+  // );
   return (
-    <div className="user-profile-box3">
-      <div className="battery-info">
-        <BatteryComponent />
-        <div className="evhealth">
-          <EVHealth healthvalue={80} />
-        </div>
+
+    <div className="box3">
+      <div className="box3-card-1">
+      <BatteryComponent />
       </div>
-      <div className="energy-usage">
-        <LastChargeInfo/>
-        <ChargeHistory/>
+      <div className="box3-card-1">
+        
+      </div>
+      <div className="box3-card-1">
+      <div className="evhealth">
+         <EVHealth healthvalue={80} />
+      </div>
+      </div>
+      <div className="box3-card-1">
       </div>
     </div>
   );
