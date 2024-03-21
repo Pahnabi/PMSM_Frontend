765c433873c42a03b24262aee740207cb642d9ee
MasterEzio <sushant.singh.7326@gmail.com>
2024-03-21 14:37:17 +0530

    box4 fixed

--- pmsm_front/src/components/UserProfile/UserProfile2.js
+++ pmsm_front/src/components/UserProfile/UserProfile2.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./UserProfile2.css";
 import Box3 from "./blocks/Box3";
 import Box4 from "./blocks/Box4";
+
+import CurrentGraph from "./blocks/CurrentGraph";
+import FreqGraph from "./blocks/FreqGraph";
 import CarImage from "./blocks/CarImage";
 import DashboardMap from "./blocks/DashboardMap";
 
@@ -9,7 +12,6 @@ const UserProfile2 = () => {
   const position = [51.505, -0.09];
 
   return (
-
     <div className="user-profile-2">
       <div className="user-profile-2-card-1">
         <CarImage />
@@ -21,7 +23,15 @@ const UserProfile2 = () => {
         <Box3 />
       </div>
       <div className="user-profile-2-card-1">
-        <Box4 />
+        {/* <Box4 /> */}
+        <div className="box4">
+          <div className="cr-graph">
+            <CurrentGraph />
+          </div>
+          <div className="fr-graph">
+            <FreqGraph />
+          </div>
+        </div>
       </div>
     </div>
   );
