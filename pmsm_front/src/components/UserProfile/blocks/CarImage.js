99fd8c393c11b0b97fb162a88f4486290e101926
DikshantSh <dikshant.2412@kgpian.iitkgp.ac.in>
2024-03-21 14:53:48 +0530

    .

--- pmsm_front/src/components/UserProfile/blocks/CarImage.js
+++ pmsm_front/src/components/UserProfile/blocks/CarImage.js
@@ -3,14 +3,19 @@ import "./CarImage.css";
 import carimg from "../../../Media/car5.png";
 
 const CarImage = () => {
-    return (
-        <div className="car-image-box">
-          <div className="car-image-box-overlay-text">
-            Hello <span>User</span>
-          </div>
-          <img src={carimg} />
-        </div>
-    )
+  return (
+    <div className="car-image-box">
+      <div className="car-image-box-overlay-text">
+        <h6>Hello User</h6>
+        {/* <h6>Hello User</h6> */}
+        <img src={carimg} />
+      </div>
+      <div className="car-image">
+        <img src={carimg} />
+        {/* <h6>Hello User</h6> */}
+      </div>
+    </div>
+  );
 };
 
 export default CarImage;
