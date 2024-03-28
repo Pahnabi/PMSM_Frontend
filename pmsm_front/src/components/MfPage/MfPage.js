6730214c306c97757f3b5a8a6681a1db447dd7d5
MasterEzio <sushant.singh.7326@gmail.com>
2024-03-28 14:18:35 +0530

    current and freq graph on mf page

--- pmsm_front/src/components/MfPage/MfPage.js
+++ pmsm_front/src/components/MfPage/MfPage.js
@@ -1,21 +1,33 @@
 import React from "react";
-import "./MfPage.css"
+import "./MfPage.css";
 import MfCarImg from "./blocks/MfCarImg";
+import GraphCurrent from "./blocks/GraphCurrent";
+import GraphFreq from "./blocks/GraphFreq";
 import Box3 from "../UserProfile/blocks/Box3";
-const MfPage = () =>{
-    return(
-        <div className="mf-page">
-            <div className="mf-page-card-1">
-                <MfCarImg/>
-            </div>
-            <div className="mf-page-card-1"></div>
-            <div className="mf-page-card-1">
-                <Box3/>
-            </div>
-            <div className="mf-page-card-1"></div>
+// import Box2 from "./blocks/Box2";
 
+const MfPage = () => {
+  return (
+    <div className="mf-page">
+      <div className="mf-page-card-1">
+        <MfCarImg />
+      </div>
+      <div className="mf-page-card-1">
+        <div className="box2">
+          <div className="mf-cr-graph">
+            <GraphCurrent />
+          </div>
+          <div className="mf-fr-graph">
+            <GraphFreq />
+          </div>
         </div>
-    );
+      </div>
+      <div className="mf-page-card-1">
+        <Box3 />
+      </div>
+      <div className="mf-page-card-1"></div>
+    </div>
+  );
 };
 
 export default MfPage;
