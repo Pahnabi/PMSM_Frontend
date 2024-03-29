c587df357cc7fee8fff0c28c09c7b69952d4de41
MasterEzio <sushant.singh.7326@gmail.com>
2024-03-29 18:15:10 +0530

    removed import displaycard

--- pmsm_front/src/components/MfPage/MfPage.js
+++ pmsm_front/src/components/MfPage/MfPage.js
@@ -6,7 +6,6 @@ import GraphFreq from "./blocks/GraphFreq";
 import Box3 from "../UserProfile/blocks/Box3";
 import FaultHistory from "./blocks/FaultHistory";
 // import MfBox4 from "./blocks/MfBox4";
-import DisplayCard from "./blocks/DisplayCard";
 import ServiceHistory from "./blocks/ServiceHistory";
 
 const MfPage = () => {
@@ -32,10 +31,6 @@ const MfPage = () => {
         <div className="mf-page-box-4">
           <div className="mf-page-card-2">
             <FaultHistory />
-            {/* <DisplayCard description='Frequency Overloaded' timestamp='Thu, 28 Mar 2024 14:17:03 GMT'/> */}
-            {/* <DisplayCard description='Current Overloaded' timestamp='Thu, 28 Mar 2024 14:17:11 GMT'/>
-            <DisplayCard description='Current Overloaded' timestamp='Thu, 28 Mar 2024 15:14:26 GMT'/>
-            <DisplayCard description='hello' timestamp='hello'/> */}
           </div>
           <div className="mf-page-card-2">
             <ServiceHistory/>
