42ed282d65f9dcf4724f662d402253a02f27207b
MasterEzio <sushant.singh.7326@gmail.com>
2024-03-29 19:27:05 +0530

    display card changes - margin and max-width

--- pmsm_front/src/components/MfPage/blocks/DisplayCard.js
+++ pmsm_front/src/components/MfPage/blocks/DisplayCard.js
@@ -4,9 +4,9 @@ const DisplayCard = ({description, timestamp}) => {
 
 
   return (
-    <div className="display-card" style={{marginBlock :'1rem'}}>
+    <div className="display-card" style={{marginBottom :'1rem'}}>
       <div
-        className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+        className="block max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
       >
         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {description}
