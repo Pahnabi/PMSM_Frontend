898b6548e56b2e97f6dc1b1ed204bfcc7dc4b288
MasterEzio <sushant.singh.7326@gmail.com>
2024-03-29 19:28:52 +0530

    external css removed from fault and service history

--- pmsm_front/src/components/MfPage/blocks/ServiceHistory.js
+++ pmsm_front/src/components/MfPage/blocks/ServiceHistory.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./FaultHistory.css";
+// import "./FaultHistory.css";
 import axios from "axios";
 import DisplayCard from "./DisplayCard";
 
