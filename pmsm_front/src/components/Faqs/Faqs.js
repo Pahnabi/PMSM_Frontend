43e564a0ba67162631eb27c04c3f385220b00f83
Pahnabi <108418535+Pahnabi@users.noreply.github.com>
2024-04-04 15:11:28 +0530

    Update Faqs.js

--- pmsm_front/src/components/Faqs/Faqs.js
+++ pmsm_front/src/components/Faqs/Faqs.js
@@ -1,22 +1,39 @@
 import React from 'react';
+import {useState} from 'react'
 import "./Faqs.css";
 
 function Faqs() {
-    return (
-      <div className="faqs-page">
-        <h1
-          className="faqs-heading">
-          Frequently Asked Questions
-        </h1>
-        <p
-          className="faqs-content "
-        >
-          Here are some of the most frequently asked questions about our services. If you have any other questions, feel free to contact us.
-        </p>
-        <div className="image-faqs"></div>
+    const [selected,setSelected]=useState(null)
+  const toggle=(i)=>{
+    if(selected==i){
+      return setSelected(null)
+    }
+
+    setSelected(i)
+  }
+
+  return (
+    <div className='wrapper'>
+      <div className='faqPage'>
+        {data.map((item,i)=>(
+          <div className='item'>
+            <div className='title' onClick={()=>toggle(i)}>
+              <h2>{item.question}</h2>
+              <span>{selected ==i ? '-':'+'}</span>
+            </div>
+            <div className={selected ==i ? 'content show':'content'}>{item.answer}</div>
+          </div>
+        ))}
       </div>
-    );
+    </div>
+  );
 }
 
+const data=[
+  {question:'Question 1',answer:'Lorem ipsum dolor sit amet.'},
+  {question:'Question 2',answer:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae unde commodi deleniti, impedit dignissimos at!'},
+  {question:'Question 3',answer:'lorem'},
+  {question:'Question 4',answer:'lorem'},
+]
 
 export default Faqs;
